7f9062eb29c7c2b75202ed891991837720d447bc
Blinkz-AL <erdem-300@hotmail.com>
2024-04-08 12:18:30 +0300

    Complete Setup

--- Basic Stock Program/Form1.Designer.cs
+++ Basic Stock Program/Form1.Designer.cs
@@ -29,13 +29,337 @@ namespace Basic_Stock_Program
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.textBox9 = new System.Windows.Forms.TextBox();
+            this.textBox10 = new System.Windows.Forms.TextBox();
+            this.textBox11 = new System.Windows.Forms.TextBox();
+            this.textBox12 = new System.Windows.Forms.TextBox();
+            this.label13 = new System.Windows.Forms.Label();
+            this.label14 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(290, 56);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(136, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Leave += new System.EventHandler(this.textBox1_Leave);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(488, 56);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(136, 20);
+            this.textBox2.TabIndex = 1;
+            this.textBox2.Leave += new System.EventHandler(this.textBox2_Leave);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(727, 44);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(171, 31);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Stok Miktarı: ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(21, 53);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(195, 31);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "BIFF SNADER";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(21, 136);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(250, 31);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "KYLLING SNADER";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(21, 219);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(167, 31);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "LAHMACUN";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.Location = new System.Drawing.Point(21, 315);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(222, 31);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "LAMM KOTALET";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label6.Location = new System.Drawing.Point(21, 413);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(100, 31);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "ET ŞİŞ";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label7.Location = new System.Drawing.Point(21, 515);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(156, 31);
+            this.label7.TabIndex = 9;
+            this.label7.Text = "TAVUK ŞİŞ";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label8.Location = new System.Drawing.Point(727, 127);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(171, 31);
+            this.label8.TabIndex = 10;
+            this.label8.Text = "Stok Miktarı: ";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label9.Location = new System.Drawing.Point(727, 208);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(171, 31);
+            this.label9.TabIndex = 11;
+            this.label9.Text = "Stok Miktarı: ";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label10.Location = new System.Drawing.Point(727, 305);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(171, 31);
+            this.label10.TabIndex = 12;
+            this.label10.Text = "Stok Miktarı: ";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label11.Location = new System.Drawing.Point(727, 412);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(171, 31);
+            this.label11.TabIndex = 13;
+            this.label11.Text = "Stok Miktarı: ";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label12.Location = new System.Drawing.Point(727, 514);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(171, 31);
+            this.label12.TabIndex = 14;
+            this.label12.Text = "Stok Miktarı: ";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(290, 139);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(136, 20);
+            this.textBox3.TabIndex = 15;
+            this.textBox3.Leave += new System.EventHandler(this.textBox3_Leave);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(488, 139);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(136, 20);
+            this.textBox4.TabIndex = 16;
+            this.textBox4.Leave += new System.EventHandler(this.textBox4_Leave);
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(290, 219);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(136, 20);
+            this.textBox5.TabIndex = 17;
+            this.textBox5.Leave += new System.EventHandler(this.textBox5_Leave);
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(488, 219);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(136, 20);
+            this.textBox6.TabIndex = 18;
+            this.textBox6.Leave += new System.EventHandler(this.textBox6_Leave);
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(290, 315);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(136, 20);
+            this.textBox7.TabIndex = 19;
+            this.textBox7.Leave += new System.EventHandler(this.textBox7_Leave);
+            // 
+            // textBox8
+            // 
+            this.textBox8.Location = new System.Drawing.Point(488, 316);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(136, 20);
+            this.textBox8.TabIndex = 20;
+            this.textBox8.Leave += new System.EventHandler(this.textBox8_Leave);
+            // 
+            // textBox9
+            // 
+            this.textBox9.Location = new System.Drawing.Point(290, 424);
+            this.textBox9.Name = "textBox9";
+            this.textBox9.Size = new System.Drawing.Size(136, 20);
+            this.textBox9.TabIndex = 21;
+            this.textBox9.Leave += new System.EventHandler(this.textBox9_Leave);
+            // 
+            // textBox10
+            // 
+            this.textBox10.Location = new System.Drawing.Point(488, 424);
+            this.textBox10.Name = "textBox10";
+            this.textBox10.Size = new System.Drawing.Size(136, 20);
+            this.textBox10.TabIndex = 22;
+            this.textBox10.Leave += new System.EventHandler(this.textBox10_Leave);
+            // 
+            // textBox11
+            // 
+            this.textBox11.Location = new System.Drawing.Point(290, 526);
+            this.textBox11.Name = "textBox11";
+            this.textBox11.Size = new System.Drawing.Size(136, 20);
+            this.textBox11.TabIndex = 23;
+            this.textBox11.Leave += new System.EventHandler(this.textBox11_Leave);
+            // 
+            // textBox12
+            // 
+            this.textBox12.Location = new System.Drawing.Point(488, 526);
+            this.textBox12.Name = "textBox12";
+            this.textBox12.Size = new System.Drawing.Size(136, 20);
+            this.textBox12.TabIndex = 24;
+            this.textBox12.Leave += new System.EventHandler(this.textBox12_Leave);
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label13.Location = new System.Drawing.Point(304, 9);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(112, 31);
+            this.label13.TabIndex = 25;
+            this.label13.Text = "Eklenen";
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label14.Location = new System.Drawing.Point(494, 9);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(120, 31);
+            this.label14.TabIndex = 26;
+            this.label14.Text = "Çıkarılan";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(1052, 630);
+            this.Controls.Add(this.label14);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.textBox12);
+            this.Controls.Add(this.textBox11);
+            this.Controls.Add(this.textBox10);
+            this.Controls.Add(this.textBox9);
+            this.Controls.Add(this.textBox8);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "İstanbul Karkuk Restoran";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.TextBox textBox9;
+        private System.Windows.Forms.TextBox textBox10;
+        private System.Windows.Forms.TextBox textBox11;
+        private System.Windows.Forms.TextBox textBox12;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label label14;
     }
 }
 
--- Basic Stock Program/Form1.cs
+++ Basic Stock Program/Form1.cs
@@ -12,9 +12,166 @@ namespace Basic_Stock_Program
 {
     public partial class Form1 : Form
     {
+        private int stokMiktari = 0;
+        private int stokmiktari1 = 0;
+        private int stokmiktari2 = 0;
+        private int stokmiktari3 = 0;
+        private int stokmiktari4 = 0;
+        private int stokmiktari5 = 0;
         public Form1()
         {
             InitializeComponent();
+            GuncelleStokLabel();
+        }
+        private void GuncelleStokLabel()
+        {
+            label1.Text = "Stok Miktarı: " + stokMiktari.ToString();
+            label8.Text = "Stok Miktarı: " + stokmiktari1.ToString();
+            label9.Text = "Stok Miktarı: " + stokmiktari2.ToString();
+            label10.Text = "Stok Miktarı: " + stokmiktari3.ToString();
+            label11.Text = "Stok Miktarı: " + stokmiktari4.ToString();
+            label12.Text = "Stok Miktarı: " + stokmiktari5.ToString();
+
+        }
+        private void textBox1_Leave(object sender, EventArgs e)
+        {
+            // TextBox'tan değeri alıyoruz
+            if (int.TryParse(textBox1.Text, out int girilenMiktar))
+            {
+                // TextBox'tan çıkıldığında, bu değeri mevcut stok miktarına ekleyip güncelliyoruz
+                stokMiktari += girilenMiktar;
+                GuncelleStokLabel();
+                textBox1.Text = "";
+            }
+        }
+
+        private void textBox2_Leave(object sender, EventArgs e)
+        {
+            // TextBox2'den değeri alıyoruz
+            if (int.TryParse(textBox2.Text, out int cikarilacakMiktar))
+            {
+                // TextBox2'den çıkıldığında, bu değeri mevcut stok miktarından çıkarıp güncelliyoruz
+                stokMiktari -= cikarilacakMiktar;
+                GuncelleStokLabel();
+                textBox2.Text = "";
+            }
+        }
+
+        private void textBox3_Leave(object sender, EventArgs e)
+        {
+            if (int.TryParse(textBox3.Text, out int girilenMiktar))
+            {
+                // TextBox'tan çıkıldığında, bu değeri mevcut stok miktarına ekleyip güncelliyoruz
+                stokmiktari1 += girilenMiktar;
+                GuncelleStokLabel();
+                textBox3.Text = "";
+            }
+        }
+
+        private void textBox4_Leave(object sender, EventArgs e)
+        {
+            // TextBox2'den değeri alıyoruz
+            if (int.TryParse(textBox4.Text, out int cikarilacakMiktar))
+            {
+                // TextBox2'den çıkıldığında, bu değeri mevcut stok miktarından çıkarıp güncelliyoruz
+                stokmiktari1 -= cikarilacakMiktar;
+                GuncelleStokLabel();
+                textBox4.Text = "";
+            }
+        }
+
+     
+
+        private void textBox6_Leave(object sender, EventArgs e)
+        {
+            // TextBox2'den değeri alıyoruz
+            if (int.TryParse(textBox6.Text, out int cikarilacakMiktar))
+            {
+                // TextBox2'den çıkıldığında, bu değeri mevcut stok miktarından çıkarıp güncelliyoruz
+                stokmiktari2 -= cikarilacakMiktar;
+                GuncelleStokLabel();
+                textBox6.Text = "";
+            }
+        }
+
+        private void textBox7_Leave(object sender, EventArgs e)
+        {
+            if (int.TryParse(textBox7.Text, out int girilenMiktar))
+            {
+                // TextBox'tan çıkıldığında, bu değeri mevcut stok miktarına ekleyip güncelliyoruz
+                stokmiktari3 += girilenMiktar;
+                GuncelleStokLabel();
+                textBox7.Text = "";
+            }
+        }
+
+        private void textBox8_Leave(object sender, EventArgs e)
+        {
+            // TextBox2'den değeri alıyoruz
+            if (int.TryParse(textBox8.Text, out int cikarilacakMiktar))
+            {
+                // TextBox2'den çıkıldığında, bu değeri mevcut stok miktarından çıkarıp güncelliyoruz
+                stokmiktari3 -= cikarilacakMiktar;
+                GuncelleStokLabel();
+                textBox8.Text = "";
+            }
+        }
+
+        private void textBox9_Leave(object sender, EventArgs e)
+        {
+            if (int.TryParse(textBox9.Text, out int girilenMiktar))
+            {
+                // TextBox'tan çıkıldığında, bu değeri mevcut stok miktarına ekleyip güncelliyoruz
+                stokmiktari4 += girilenMiktar;
+                GuncelleStokLabel();
+                textBox9.Text = "";
+            }
+        }
+
+        private void textBox10_Leave(object sender, EventArgs e)
+        {
+            // TextBox2'den değeri alıyoruz
+            if (int.TryParse(textBox10.Text, out int cikarilacakMiktar))
+            {
+                // TextBox2'den çıkıldığında, bu değeri mevcut stok miktarından çıkarıp güncelliyoruz
+                stokmiktari4 -= cikarilacakMiktar;
+                GuncelleStokLabel();
+                textBox10.Text = "";
+            }
+        }
+
+        private void textBox11_Leave(object sender, EventArgs e)
+        {
+            if (int.TryParse(textBox11.Text, out int girilenMiktar))
+            {
+                // TextBox'tan çıkıldığında, bu değeri mevcut stok miktarına ekleyip güncelliyoruz
+                stokmiktari5 += girilenMiktar;
+                GuncelleStokLabel();
+                textBox11.Text = "";
+            }
+        }
+
+        private void textBox12_Leave(object sender, EventArgs e)
+        {
+            // TextBox2'den değeri alıyoruz
+            if (int.TryParse(textBox12.Text, out int cikarilacakMiktar))
+            {
+                // TextBox2'den çıkıldığında, bu değeri mevcut stok miktarından çıkarıp güncelliyoruz
+                stokmiktari5 -= cikarilacakMiktar;
+                GuncelleStokLabel();
+                textBox12.Text = "";
+            }
+        }
+
+        private void textBox5_Leave(object sender, EventArgs e)
+        {
+            if (int.TryParse(textBox5.Text, out int girilenMiktar))
+            {
+                // TextBox'tan çıkıldığında, bu değeri mevcut stok miktarına ekleyip güncelliyoruz
+                stokmiktari2 += girilenMiktar;
+                GuncelleStokLabel();
+                textBox5.Text = "";
+            }
         }
     }
 }
